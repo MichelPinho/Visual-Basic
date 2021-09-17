7377bea6997ec0a32fa0738896237111037ac4bd
Michel Pinho <jologpinho@gmail.com>
2021-09-17 19:12:55 -0300

    Delete project.assets.json

--- Primeiro-Projeto/Primeiro-Projeto/Variaveis.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Variaveis
-    Sub main() 'sempre precisa ter a declaração desse método em todas as classses.'
-        Dim info As String 'Declaração de variável '
-        Console.Write("Entre com a informação") 'Função que escreve no console '
-        info = Console.ReadLine() 'leitura da variável no console'
-    End Sub
-End Class
